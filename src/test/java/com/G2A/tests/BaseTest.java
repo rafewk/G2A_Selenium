9ad3838f26b5d65958dc5d7ebc751921b3c68a4f
rafewk <97rafal97@gmail.com>
2022-01-30 16:33:26 +0100

    add BaseTest

---
+++ src/test/java/com/G2A/tests/BaseTest.java
@@ -0,0 +1,26 @@
+package com.G2A.tests;
+
+import com.G2A.utils.Browsers;
+import com.G2A.utils.DriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        driver = DriverFactory.getDriver(Browsers.FIREFOX);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get("https://www.g2a.com/");
+    }
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+}
